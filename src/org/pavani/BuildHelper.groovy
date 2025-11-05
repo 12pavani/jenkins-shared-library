29bd76b730fcc6019d8570d1958ce38236334953
Pavani V <vpavan@sutherlandglobal.com>
2025-11-05 18:49:42 +0530

    Added BuildHelper and buildCode files

---
+++ src/org/pavani/BuildHelper.groovy
@@ -0,0 +1,25 @@
+package org.pavani
+
+class BuildHelper implements Serializable {
+    def steps
+
+    BuildHelper(steps) {
+        this.steps = steps
+    }
+
+    def buildProject() {
+        steps.echo "Starting build process..."
+        steps.echo "Cleaning workspace..."
+        steps.sh "rm -rf *"
+
+        steps.echo "Cloning the repository..."
+        steps.git branch: 'master', url: 'https://github.com/jenkins-docs/simple-java-maven-app.git'
+
+        steps.echo "Building the project using Maven..."
+        steps.withMaven(maven: 'Maven 3.9.9') {
+            steps.sh "mvn -B -DskipTests clean package"
+        }
+
+        steps.echo "Build completed successfully!"
+    }
+}
--- vars/buildCode.groovy
+++ vars/buildCode.groovy
@@ -1,16 +1,4 @@
 def call() {
-    echo "Starting build process..."
-
-    echo "Cleaning workspace..."
-    sh "rm -rf *"
-
-    echo "Cloning the repository..."
-    git branch: 'master', url: 'https://github.com/jenkins-docs/simple-java-maven-app.git'
-
-    echo "Building the project using Maven..."
-    withMaven(maven: 'Maven 3.9.9') {
-        sh "mvn -B -DskipTests clean package"
-    }
-
-    echo "Build completed successfully!"
+    def helper = new org.pavani.BuildHelper(this)
+    helper.buildProject()
 }
