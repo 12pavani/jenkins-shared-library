c5fe29d25628c0a1a9aa3d90d2d14f52e8c95748
Pavani V <vpavan@sutherlandglobal.com>
2025-11-07 19:19:55 +0530

    changes in SearchPomHelper.groovy

---
+++ src/org/pavani/Clone3Maven.groovy
@@ -0,0 +1,26 @@
+package org.package
+
+class Clone3Maven implements Serializable {
+    def mvnHome = tool name: 'Maven 3.9.9', type: 'maven'
+    def steps
+
+    Clone3Maven(steps) {
+        this.steps = steps
+    }
+
+    def cleanWorkspace() {
+        steps.echo "Cleaning workspace"
+        steps.sh "rm -rf *"
+    }
+
+    def cloneThreeRepo(repoNumber, repoName) {
+        steps.echo "Cloning Maven project ${repoNumber}"
+        steps.sh "git clone -b main https://github.com/12pavani/${repoName}.git"
+        
+        steps.echo "Building Maven project ${repoNumber}"
+        steps.sh """
+            cd ${repoName}
+            $mvnHome/bin/mvn -B -DskipTests clean package
+        """
+    }
+}
--- src/org/pavani/SearchPomHelper.groovy
+++ src/org/pavani/SearchPomHelper.groovy
@@ -1,2 +1,34 @@
 package org.pavani
 
+class SearchPomHelper implements Serializable {
+    def steps
+
+    SearchPomHelper(steps) {
+        this.steps = steps
+    }
+
+    def cleanWorkspace() {
+        steps.echo "Cleaning workspace..."
+        steps.sh 'rm -rf'
+    }
+
+    def cloneRepo() {
+        steps.echo "Cloning the repository..."
+        steps.git branch: 'master', url: 'https://github.com/sharmar0790/spring-boot-multi-module-maven.git'
+    }
+
+    def checkPomXml() {
+        def pomFile = sh(script: "find . -name 'pom.xml'", returnStdout: true).trim()
+
+        if (pomFile) {
+            echo "Pom.xml found at: ${pomFile}"
+            echo "Starting Maven build..."
+
+            withMaven(maven: 'Maven 3.9.9') {
+                sh "mvn -B -DskipTests clean package"
+            }
+        } else {
+            echo "No pom.xml file found in any subdirectory."
+        }
+    }
+}
