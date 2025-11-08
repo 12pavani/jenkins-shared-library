6a0755c1fd9a3b845c111ecec44ccc733519edd1
Pavani V <vpavan@sutherlandglobal.com>
2025-11-08 13:14:32 +0530

    changes in RepoHelper.groovy

--- src/org/pavani/RepoHelper.groovy
+++ src/org/pavani/RepoHelper.groovy
@@ -12,10 +12,9 @@ void test(String repoName, String repoUrl, String branch = 'main') {
 
         echo "Building the project using Maven..."
         withMaven(maven: 'Maven 3.9.9') {
-            sh "mvn demo-clone-two-repo/pom.xml -B -DskipTests clean package"
+            sh "mvn -f pom.xml -B -DskipTests clean package"
         }
 
         echo "Build for ${repoName} completed successfully!"
     }
 }
-
