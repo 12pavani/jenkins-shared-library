b73c7b6d08aba19abaaeaadaaf656ff12505cde1
Pavani V <vpavan@sutherlandglobal.com>
2025-11-10 11:32:27 +0530

    changes in WebhookHelper

--- src/org/pavani/WebhookHelper.groovy
+++ src/org/pavani/WebhookHelper.groovy
@@ -2,28 +2,27 @@ package org.pavani
 
 void test(String repoName, String repoUrl, String branch = 'main') {
     node {
-    properties([
-        pipelineTriggers([githubPush()])
-    ])
+        properties([
+            pipelineTriggers([githubPush()])
+        ])
 
-    stage('Checkout Main') {
-        git url: ${repoUrl}, branch: ${branch}
-    }
+        stage('Checkout Main') {
+            git url: "${repoUrl}", branch: "${branch}"
+        }
 
-    stage('Check Merge Conflicts') {
-        sh 'git fetch origin pavani'
+        stage('Check Merge Conflicts') {
+            sh "git fetch origin ${branch}"
+            sh "git merge --no-commit origin/${branch} || true"
 
-        sh 'git merge --no-commit origin/pavani || true'
+            def conflicts = sh(script: "git diff --name-only --diff-filter=U", returnStdout: true).trim()
 
-        def conflicts = sh(script: "git diff --name-only --diff-filter=U", returnStdout: true).trim()
-
-        if (conflicts) {
-            echo "Merge conflicts found in these files:"
-            echo conflicts
-        } else {
-            echo "No conflicts"
+            if (conflicts) {
+                echo "Merge conflicts found in these files:"
+                echo conflicts
+                sh 'git merge --abort'
+            } else {
+                echo "No conflicts"
+            }
         }
     }
-}
-
 }
