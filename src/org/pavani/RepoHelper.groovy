aa63375707f91e4d96aeaf7ad499e739134c1522
Pavani V <vpavan@sutherlandglobal.com>
2025-11-10 18:07:08 +0530

    commit-6

--- src/org/pavani/RepoHelper.groovy
+++ src/org/pavani/RepoHelper.groovy
@@ -4,19 +4,38 @@ package org.pavani
 
 void test(String repoName, String repoUrl, String branch = 'main') {
     node {
-        echo "Starting build process for ${repoName}..."
+    echo "Starting build process for GitDemo-1..."
 
-        echo "Cleaning workspace..."
-        sh "rm -rf *"
+    echo "Cleaning workspace..."
+    sh "rm -rf GitDemo-1"
 
-        echo "Cloning the repository..."
-        sh "git clone -b ${branch} ${repoUrl} ${repoName}"
+    echo "Cloning the repository..."
+    sh "git clone -b master https://github.com/jenkins-docs/simple-java-maven-app.git GitDemo-1"
 
-        echo "Building the project using Maven..."
-        withMaven(maven: 'Maven 3.9.9') {
-            sh "mvn -f ${repoName}/pom.xml -B -DskipTests clean package"
-        }
-
-        echo "Build for ${repoName} completed successfully!"
+    echo "Building the project using Maven..."
+    withMaven(maven: 'Maven 3.9.9') {
+        sh "mvn -f GitDemo-1/pom.xml -B -DskipTests clean package"
     }
+
+    echo "Build for GitDemo-1 completed successfully!"
+
+    echo "Starting build process for GitDemo-2..."
+
+    echo "Cleaning workspace..."
+    sh "rm -rf GitDemo-2"
+
+    echo "Cloning the repository..."
+    sh "git clone -b master https://github.com/jenkins-docs/simple-java-maven-app.git GitDemo-2"
+
+    echo "Building the project using Maven..."
+    withMaven(maven: 'Maven 3.9.9') {
+        sh "mvn -f GitDemo-2/pom.xml -B -DskipTests clean package"
+    }
+
+    echo "Build for GitDemo-2 completed successfully!"
+
+    echo "Cleaning workspace..."
+    sh "rm -rf GitDemo-1 GitDemo-2"
+}
+
 }
