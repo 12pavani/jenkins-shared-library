a432e347085ad3ec9593da0877c671013a79cf1a
Pavani V <vpavan@sutherlandglobal.com>
2025-11-10 17:36:33 +0530

    commit-2

--- src/org/pavani/BuildHelper.groovy
+++ src/org/pavani/BuildHelper.groovy
@@ -4,6 +4,9 @@ void test(String repoName, String repoUrl, String branch = 'main') {
     node {
         echo "Starting build process for ${repoName}..."
         
+        echo "Cleaning workspace..."
+        sh "rm -rf *"
+        
         echo "Cloning the repository..."
         git branch: branch, url: repoUrl
         
@@ -11,9 +14,6 @@ void test(String repoName, String repoUrl, String branch = 'main') {
         withMaven(maven: 'Maven 3.9.9') {
             sh "mvn -B -DskipTests clean package"
         }
-
-        echo "Removing cloned repository..."
-        sh "rm -rf ${repoName}"  
 
         echo "Build for ${repoName} completed successfully!"
     }
--- src/org/pavani/Clone3Maven.groovy
+++ src/org/pavani/Clone3Maven.groovy
@@ -5,15 +5,19 @@ package org.pavani
 void test(String repoName, String repoUrl, String branch = 'main') {
     node {
         echo "Cleaning workspace..."
-        sh "rm -rf *"
+        deleteDir()
 
-        stage("Building ${repoName}") {
+        stage("Building Mavenrepo-1") {
+            def repoName = 'Mavenrepo-1'
+            def repoUrl = 'https://github.com/12pavani/Leave_Management_Backend.git'
+            def branch = 'main'
+
             echo "Cloning ${repoUrl} ..."
             sh "git clone -b ${branch} ${repoUrl} ${repoName}"
-            def pomFiles1 = sh(script: "find ${repoName} -name 'pom.xml'", returnStdout: true).trim()
 
-            if (pomFiles1) {
-                echo "Found pom.xml files:\n${pomFiles1}"
+            def pomFiles = sh(script: "find ${repoName} -name 'pom.xml'", returnStdout: true).trim()
+            if (pomFiles) {
+                echo "Found pom.xml files:\n${pomFiles}"
                 withMaven(maven: 'Maven 3.9.9') {
                     sh "mvn -f ${repoName}/pom.xml -B -DskipTests clean install"
                 }
@@ -23,5 +27,47 @@ void test(String repoName, String repoUrl, String branch = 'main') {
             }
         }
 
+        stage("Building Mavenrepo-2") {
+            def repoName = 'Mavenrepo-2'
+            def repoUrl = 'https://github.com/sharmar0790/spring-boot-multi-module-maven.git'
+            def branch = 'master'
+
+            echo "Cloning ${repoUrl} ..."
+            sh "git clone -b ${branch} ${repoUrl} ${repoName}"
+
+            def pomFiles = sh(script: "find ${repoName} -name 'pom.xml'", returnStdout: true).trim()
+            if (pomFiles) {
+                echo "Found pom.xml files:\n${pomFiles}"
+                withMaven(maven: 'Maven 3.9.9') {
+                    sh "mvn -f ${repoName}/pom.xml -B -DskipTests clean install"
+                }
+                echo "Build for ${repoName} completed successfully!"
+            } else {
+                echo "No pom.xml file found in ${repoName}."
+            }
+        }
+
+        stage("Building Mavenrepo-3") {
+            def repoName = 'Mavenrepo-3'
+            def repoUrl = 'https://github.com/jenkins-docs/simple-java-maven-app.git'
+            def branch = 'master'
+
+            echo "Cloning ${repoUrl} ..."
+            sh "git clone -b ${branch} ${repoUrl} ${repoName}"
+
+            def pomFiles = sh(script: "find ${repoName} -name 'pom.xml'", returnStdout: true).trim()
+            if (pomFiles) {
+                echo "Found pom.xml files:\n${pomFiles}"
+                withMaven(maven: 'Maven 3.9.9') {
+                    sh "mvn -f ${repoName}/pom.xml -B -DskipTests clean install"
+                }
+                echo "Build for ${repoName} completed successfully!"
+            } else {
+                echo "No pom.xml file found in ${repoName}."
+            }
+        }
+
+        echo "All builds completed. Cleaning workspace..."
+        deleteDir()
     }
 }
