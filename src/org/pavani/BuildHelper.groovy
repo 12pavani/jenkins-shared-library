a295182c46c8a15b729d551a323e640745718d63
Pavani V <vpavan@sutherlandglobal.com>
2025-11-10 17:21:30 +0530

    commit-1

--- src/org/pavani/BuildHelper.groovy
+++ src/org/pavani/BuildHelper.groovy
@@ -4,9 +4,6 @@ void test(String repoName, String repoUrl, String branch = 'main') {
     node {
         echo "Starting build process for ${repoName}..."
         
-        echo "Cleaning workspace..."
-        sh "rm -rf *"
-        
         echo "Cloning the repository..."
         git branch: branch, url: repoUrl
         
@@ -14,6 +11,9 @@ void test(String repoName, String repoUrl, String branch = 'main') {
         withMaven(maven: 'Maven 3.9.9') {
             sh "mvn -B -DskipTests clean package"
         }
+
+        echo "Removing cloned repository..."
+        sh "rm -rf ${repoName}"  
 
         echo "Build for ${repoName} completed successfully!"
     }
